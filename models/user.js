9a806f2280ce7817df090fe490e9d6947c530550
blhl1210 <blhl1210@gmail.com>
2018-04-11 00:38:52 +0700

    Update model user

--- models/user.js
+++ models/user.js
@@ -21,8 +21,8 @@ function getUsers() {
 function addUser(paramters) {
     pool.getConnection(function (err, connection) {
         // Use the connection
-        paramters = pool.standardized(paramters,8);
-        var query = 'INSERT INTO user values(null,?,?,?,?,?,?,?,?);';
+//        paramters = pool.standardized(paramters,8);
+        var query = 'INSERT INTO user SET ?;';
         connection.query(query,paramters, function (error, results, fields) {
             // And done with the connection.
             connection.release();
@@ -34,8 +34,42 @@ function addUser(paramters) {
     });
 }
 
-addUser(['linh','123']);
+function updateUser(paramters,id) {
+    pool.getConnection(function (err, connection) {
+        // Use the connection
+//        paramters = pool.standardized(paramters,8);
+        var query =  `UPDATE user SET ? WHERE U_ID = ${id};`;
+        connection.query(query,paramters, function (error, results, fields) {
+            // And done with the connection.
+            connection.release();
+            // Handle error after the release.
+            if (error) throw error;
+            // Don't use the connection here, it has been returned to the pool.
+            console.log(results);
+        });
+    });
+}
+
+function deleteUser(id) {
+    pool.getConnection(function (err, connection) {
+        // Use the connection
+//        paramters = pool.standardized(paramters,8);
+        var query =  `Delete FROM user WHERE U_ID = ${id};`;
+        connection.query(query, function (error, results, fields) {
+            // And done with the connection.
+            connection.release();
+            // Handle error after the release.
+            if (error) throw error;
+            // Don't use the connection here, it has been returned to the pool.
+            console.log(results);
+        });
+    });
+}
+
+
 module.exports = {
     getUsers,
-    addUser
+    addUser,
+    deleteUser,
+    updateUser
 }
