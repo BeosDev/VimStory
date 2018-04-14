5570346db362efc4896ba3a7d4832d3edfb34570
blhl1210 <blhl1210@gmail.com>
2018-04-14 12:11:08 +0700

    Update

--- config/database.js
+++ config/database.js
@@ -1,19 +1,37 @@
 var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit: 10,
+var con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'vimstory'
 });
 
-pool.standardized = function (paramters,len) {
-  if (paramters.length !== len) {
-    for (var i = paramters.length; i < len; i++) {
-      paramters[i] = null;
-    }
-  }
-  return paramters;
+function executeQuery(cmd,paramters){
+    con.connect();
+    var query = con.query(cmd,paramters);
+    con.end();
+    return query;
 }
+/*
+var query = connection.query('SELECT * FROM posts');
+query
+  .on('error', function(err) {
+    // Handle error, an 'end' event will be emitted after this as well
+  })
+  .on('fields', function(fields) {
+    // the field packets for the rows to follow
+  })
+  .on('result', function(row) {
+    // Pausing the connnection is useful if your processing involves I/O
+    connection.pause();
+ 
+    processRow(row, function() {
+      connection.resume();
+    });
+  })
+  .on('end', function() {
+    // all rows have been received
+  });
+*/
 
-module.exports = pool;
+module.exports = executeQuery;
