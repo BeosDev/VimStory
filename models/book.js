71a3ee32a41547e445bcc9e34d4f3c9e07d8777c
skynetcmg47 <skynetcmg47@gmail.com>
2018-04-15 13:37:26 +0700

    modified model book

--- models/book.js
+++ models/book.js
@@ -1,91 +1,34 @@
-var pool = require('../config/database');
-var EventEmitter = require('events').EventEmitter;
+var executeQuery = require('../config/database');
 
 function getBooks() {
-    var emitter = this;
-    pool.getConnection(function (err, connection) {
-        // Use the connection
-        var query = 'SELECT * FROM book';
-        connection.query(query , function (error, results, fields) {
-            // And done with the connection.
-            connection.release();
-
-            // Handle error after the release.
-            if (error) {
-                throw error;
-            }
-            // Don't use the connection here, it has been returned to the pool
-            emitter.emit('results',results);
-        });
-    });
+    var query = 'SELECT * FROM book';
+    return new executeQuery(query);
 }
 
-getBooks.prototype = new EventEmitter();
-
-//var k = new getBooks();
-//k.on('results',results => console.log(results));
-
-
-
-function addBook(book) {
-    pool.getConnection(function (err, connection) {
-        // Use the connection
-        //paramters = pool.standardized(paramters,8);
-        //let book = {B_Name:'hihi'};
-        var query = 'INSERT INTO book SET ?;';
-        connection.query(query,book, function (error, results, fields) {
-            // And done with the connection.
-            connection.release();
-            // Handle error after the release.
-            if (error) throw error;
-            // Don't use the connection here, it has been returned to the pool.
-            console.log(results);
-        });
-    });
+function addBook(paramters) {
+    var query = 'INSERT INTO book SET ?;';
+    return new executeQuery(query, paramters);
 }
 
-function updateBook(book,id) {
-    pool.getConnection(function (err, connection) {
-        // Use the connection
-        //paramters = pool.standardized(paramters,8);
-        //let book = {B_Name:'hihi'};
-        var query = `UPDATE book SET ? WHERE B_ID = ${id};`;
-        connection.query(query,book, function (error, results, fields) {
-            // And done with the connection.
-            connection.release();
-            // Handle error after the release.
-            if (error) throw error;
-            // Don't use the connection here, it has been returned to the pool.
-            console.log(results);
-        });
-    });
+function updateBook(paramters, id) {
+    var query = `UPDATE book SET ? WHERE B_ID = ${id};`;
+    return new executeQuery(query, paramters);
 }
 
 function deleteBook(id) {
-    pool.getConnection(function (err, connection) {
-        // Use the connection
-        //paramters = pool.standardized(paramters,8);
-        //let book = {B_Name:'hihi'};
-        var query = `DELETE FROM book WHERE B_ID = ${id};`;
-        connection.query(query, function (error, results, fields) {
-            // And done with the connection.
-            connection.release();
-            // Handle error after the release.
-            if (error) throw error;
-            // Don't use the connection here, it has been returned to the pool.
-            console.log(results);
-        });
-    });
+    var query = `Delete FROM book WHERE B_ID = ${id};`;
+    return new executeQuery(query);
 }
 
+var k = new getBooks();
+k.on('results',function(results){
+    console.log(results[0]);
+})
 
-//var B_Name = 'hihi';
-//addBook({B_Name:'Linh123456'});
-//var B_ID = 7;
-//var B_Name = 'asdsad';
-//deleteBook(B_ID)
 module.exports = {
     getBooks,
     addBook,
-    deleteBook
+    deleteBook,
+    updateBook
 }
+
