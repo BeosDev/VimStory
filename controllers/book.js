08706248e980a60887d506196d61adac671cf02c
blhl1210 <blhl1210@gmail.com>
2018-04-18 01:09:21 +0700

    add

--- controllers/book.js
+++ controllers/book.js
@@ -4,14 +4,8 @@ var bookModel = require('../models/book');
 
 function getBooks(req,res,next){
     var books = new bookModel.getBooks;
-<<<<<<< HEAD
     books.on('results',function (data){
         //console.log(data);
-=======
-    books.on('result',function(result){
-        
-        console.log(result[1]);
->>>>>>> nana
         res.render('admin/adminBook', { title: 'Manage book - Vimstory',data:data});
     })
 }
