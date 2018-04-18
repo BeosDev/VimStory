7b12b9386856cb567e32b98a64611d1b4adc4717
blhl1210 <blhl1210@gmail.com>
2018-04-18 07:13:30 +0700

    f*ck firebug

--- controllers/book.js
+++ controllers/book.js
@@ -2,38 +2,45 @@ var bookModel = require('../models/book');
 
 function getBooks(req, res, next) {
     var books = new bookModel.getBooks;
-    /*
-    
-    */
-        books.on('results', function (data) {
+
+    books.on('results', function (data) {
+        if (data.length > 0) {
             res.render('admin/adminBook', {
                 title: 'Manage book - Vimstory',
                 data: data
-            },function(err,html){
+            }, function (err, html) {
                 res.end(html);
-            });
-           
-    })
-    
+            })
+        }
+        else res.end('error');
+
+    });
 }
 
 function addBook(req, res, next) {
 
     var B_Name = req.body.B_Name;
     var B_Content = req.body.B_Content;
+
     var books = new bookModel.addBook({
         B_Name: B_Name,
         B_Content: B_Content
     });
-
-
+    req.isRedirect = false;
     books.on('results', function (results) {
         //       console.log(req);
         //res.send('add successfully');
-        res.redirect('/adminBook');
-        res.end();
+
+        if (results.affectedRows > 0) {
+            req.isRedirect = true;
+            next();
+        }
         //getBooks();
     })
+    books.on('error', function (err) {
+        next();
+    })
+
 }
 
 
--- routes/adminBook.js
+++ routes/adminBook.js
@@ -3,18 +3,23 @@ var router = express.Router();
 var bookController = require('../controllers/book');
 /* GET home page. */
 
-router.get('/adminBook', function(req, res) {
-   bookController.getBooks(req,res);
-   //res.render('admin/adminBook', { title: 'Add new book - Vimstory'});
+router.get('/adminBook', function (req, res) {
+    bookController.getBooks(req, res);
+    //res.render('admin/adminBook', { title: 'Add new book - Vimstory'});
 });
 
 
-router.get('/addBook', function(req, res, next) {
-    res.render('admin/addBook', { title: 'Add new book - Vimstory'});
- });
+router.get('/addBook', function (req, res, next) {
+    res.render('admin/addBook', {
+        title: 'Add new book - Vimstory'
+    });
+});
 
-router.post('/addnewbook',function(req,res,next){
-    bookController.addBook(req,res,next);
+router.post('/addnewbook', function (req, res, next) {
+    bookController.addBook(req, res, next);
+}, function (req, res, next) {
+    if (req.isRedirect === true)
+        res.redirect('./adminBook');
 });
 
 module.exports = router;
