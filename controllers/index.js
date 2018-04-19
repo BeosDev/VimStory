cc227d2e5ce3483bacd463b6fab018e1a5dcbd2f
skynetcmg47 <skynetcmg47@gmail.com>
2018-04-19 14:13:29 +0700

    update index

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,23 @@
-console.log("Index");
+var bookModel = require('../models/book');
+
+function getBooks(req, res, next) {
+    var books = new bookModel.getBooks;
+
+    books.on('results', function (data) {
+        if (data.length > 0) {
+            res.render('index', {
+                title: 'Home - VimStory',
+                data: data
+            }, function (err, html) {
+                res.end(html);
+            })
+        }
+        else res.end('error');
+
+    });
+}
+
+
+module.exports = {
+    getBooks
+}
