d3fd2521a371766b819d6d059021ec35fe2fb38a
beosdev <blhl1210@gmail.com>
2018-04-21 12:45:06 +0700

    openfpt api

--- controllers/utils/openfpt.js
+++ controllers/utils/openfpt.js
@@ -1,44 +1,57 @@
-var http = require("http");
 var EventEmitter = require('events').EventEmitter;
+var request = require("request");
 
-function SpeechToText(text) {
-    var options = {
-        "method": "POST",
-        "hostname": "api.openfpt.vn",
-        "port": null,
-        "path": "/text2speech/v4",
-        "headers": {
-            "api_key": "71b7c7d7d2764106a3b0a3f7fe0dbc71",
-            "content-type": "text/plain",
-            "cache-control": "no-cache",
+function getSpeech(text) {
+    return new Promise((resolve, reject) => {
+        var options = {
+            method: 'POST',
+            url: 'http://api.openfpt.vn/text2speech/v4',
+            headers: {
+                'cache-control': 'no-cache',
+                api_key: '71b7c7d7d2764106a3b0a3f7fe0dbc71'
+            },
+            body: text
+        };
+        request(options, function (error, response, body) {
+            if (error) {
+                reject('error');
+                throw new Error(error);
+            }
+            resolve(body);
+        });
+    })
+}
+
+function StringToArray(text) {
+    console.log(text.length);
+    var texts = [];
+    while (text.length > 2000) {
+        var index = 2000;
+        while (text.charAt(index) !== ' ' && index >= 0) {
+            index--;
         }
-    };
+        if (index > 0)
+            texts.push(text.slice(0, index));
+        text = text.slice(index + 1, text.length);
+    }
+    if (text.length > 0) {
+        texts.push(text);
+    }
+    console.log(texts.length);
+    return texts;
+}
+
+function TextToSpeech(text) {
+    var texts = StringToArray(text);
     var emitter = this;
-    var req = http.request(options, function (res) {
-        var chunks = [];
-
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            emitter.emit('data', body.toString());
-        });
-    });
-
-    req.write(text);
-    req.end();
+    var promises = [];
+    for (var i = 0; i < texts.length; i++) {
+        promises.push(getSpeech(texts[i]));
+    }
+    Promise.all(promises)
+        .then(result => emitter.emit('result', result))
+            .catch(err => emitter.emit('error'));
 }
+TextToSpeech.prototype = new EventEmitter();
 
-SpeechToText.prototype = new EventEmitter();
-
-/*
-var k = new SpeechToText('Xin chào các bạn');
-k.on('data', (data) => {
-    console.log(data);
-})
-*/
-module.exports = {
-    SpeechToText
-}
+module.exports = TextToSpeech;
