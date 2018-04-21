bf6c620a294030052fc955f03bc5a1d4ea5a1dac
beosdev <blhl1210@gmail.com>
2018-04-21 10:17:51 +0700

    add

---
+++ routes/home.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var indexController = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  indexController.getBooks(req,res,next);
+});
+
+module.exports = router;
