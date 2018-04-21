83cf14b2b18d6ce56992e004c768ecc4bfb1a1d4
beosdev <blhl1210@gmail.com>
2018-04-21 11:26:36 +0700

    add

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,4 @@ router.use('/',require('./home'));
 router.use('/login',require('./login'));
 //router.use('/book',require('./book'));
 router.use('/admin',require('./admin'));
-
 module.exports = router;
