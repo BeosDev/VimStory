a9cb6121ad603cbded94d19a91d5be86ca61189a
hoailinh1210 <builhoailinh@dtu.edu.vn>
2018-04-22 12:56:23 +0700

    add author

---
+++ models/author.js
@@ -0,0 +1,35 @@
+var executeQuery = require('../config/database');
+
+function getAuthors() {
+    var query = 'SELECT * FROM author';
+    return new executeQuery(query);
+}
+
+function getOneAuthor(id) {
+    var query = `SELECT * FROM author WHERE A_ID = ${id};`;
+    return new executeQuery(query);
+}
+
+function addAuthor(paramters) {
+    var query = 'INSERT INTO author SET ?;';
+    return new executeQuery(query, paramters);
+}
+
+function updateAuthor(paramters, id) {
+    var query = `UPDATE author SET ? WHERE A_ID = ${id};`;
+    return new executeQuery(query, paramters);
+}
+
+function deleteAuthor(id) {
+    var query = `Delete FROM author WHERE A_ID = ${id};`;
+    return new executeQuery(query);
+}
+
+module.exports = {
+    getAuthors,
+    addAuthor,
+    deleteAuthor,
+    updateAuthor,
+    getOneAuthor
+}
+
