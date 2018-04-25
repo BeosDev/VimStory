aae4323dabb483ab791c8bc03fbdd30f79760659
beosdev <blhl1210@gmail.com>
2018-04-25 20:10:01 +0700

    add conflict

--- routes/book.js
+++ routes/book.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-var bookController = require('../controllers/book');
-
-/* GET home page. */
-router.get('/read/:id', function(req, res, next) {
-    var path = 'index/readBook';
-    bookController.getOneBook(req,res,next,path);
-});
-router.get('/bookContent', function(req, res, next) {
-    //var path = 'index/readBookContent';
-    //bookController.getOneBook(req,res,next,path);
-    res.render('index/readBookContent');
-});
-
-=======
 var express = require('express');
 var router = express.Router();
 var bookController = require('../controllers/book');
@@ -34,5 +17,4 @@ router.get('/',function(req,res,next){
     bookController.searchBooks(req,res,next);
 })
 
->>>>>>> tim
 module.exports = router;
