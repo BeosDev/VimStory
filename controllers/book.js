b3d9e6f150b619703a357c2f1ab9e93e3360c251
magicpro97 <linhnt99x@gmail.com>
2018-04-25 21:29:02 +0700

    update search

--- controllers/book.js
+++ controllers/book.js
@@ -270,15 +270,21 @@ var authorModel = require('../models/author');
  function searchBooks(req,res,next){
     var name = req.query['search'];
     var books = bookModel.searchBooks(name);
-    books.once('results', function(results){
-        res.render('index/searchBook',{
-            title: 'Search book - Vimstory',
-            data : results
+    var categories = categoryModel.getCategories();
+
+    categories.once('results', function(ca){
+        books.once('results', function(results){
+            res.render('index/searchBook',{
+                title: 'Search book - Vimstory',
+                categories: ca,
+                data : results
+            })
+        });
+        books.once('error',function(err){
+            res.end('err');
         })
     });
-    books.once('error',function(err){
-        res.end('err');
-    })
+    
 }
  module.exports = {
     getBooks,
