507d378225d452212b81370a7204bc319b17044f
beosdev <blhl1210@gmail.com>
2018-04-25 20:16:33 +0700

    add conflict

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var router = require('express').Router();;
 var authAdmin = require('../auth').authAdmin;
 
@@ -8,15 +7,4 @@ router.use('/books',require('./books.admin'));
 router.use('/categories',require('./category.admin'));
 router.use('/users',require('./users.admin'));
 
-=======
-var router = require('express').Router();;
-var authAdmin = require('../auth').authAdmin;
-
-//router.use(authAdmin);
-
-router.use('/books',require('./books.admin'));
-router.use('/category',require('./category.admin'));
-router.use('/users',require('./users.admin'));
-
->>>>>>> develope
 module.exports = router;
