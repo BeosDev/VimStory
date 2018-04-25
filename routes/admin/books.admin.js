1961befa13c1aa1c87a5d7c9968363477aff4a74
skynetcmg47 <skynetcmg47@gmail.com>
2018-04-25 10:07:25 +0700

    up

--- routes/admin/books.admin.js
+++ routes/admin/books.admin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 var bookController = require('../../controllers/book');
@@ -37,46 +36,3 @@ router.get('/delete/:id', function (req, res, next) {
 });
 
 module.exports = router;
-=======
-var express = require('express');
-var router = express.Router();
-var bookController = require('../../controllers/book');
-/* GET home page. */
-
-router.get('/', function (req, res) {
-    bookController.getBooks(req, res);
-    //res.render('admin/adminBook', { title: 'Add new book - Vimstory'});
-});
-
-
-router.get('/add', function (req, res, next) {
-    bookController.getAddBookPage(req,res,next);
-    
-});
-
-router.post('/add', function (req, res, next) {
-    bookController.addBook(req, res, next);
-});
-
-
-router.get('/update/:id', function (req, res, next) {
-    res.render('admin/updateBook', {
-        title: 'updatebook - Vimstory',
-        id:req.params.id
-    });
-});
-
-router.post('/update',function(req,res,next){
-    bookController.updateBook(req,res,next);
-});
-
-router.get('/delete/:id', function (req, res, next) {
-    //console.log('route'+req.params.id);
-    bookController.deleteBook(req,res,next);
-}, function (req, res, next) {
-    if (req.isRedirect === true)
-        res.redirect('/admin/books');
-});
-
-module.exports = router;
->>>>>>> master
