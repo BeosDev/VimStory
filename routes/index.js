e593f36625a62e2bc11955ddb9eb819f3b75675b
phuongnamcmu <phuongnamcmg47@gmail.com>
2018-07-26 11:22:21 +0700

    add user manage book page bk

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,10 @@ router.use('/admin',require('./admin'));
 router.get('/profile', function(req, res) {
     userController.getOneUser(req,res);
   });
+
+  //
+  var bookController = require('../controllers/book');
+  router.get('/user/books', function(req, res) {
+    bookController.getUserBooks(req,res);
+  });
 module.exports = router;
