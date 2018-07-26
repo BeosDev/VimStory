4b00e992a251a6f0edfe6838ded2cfdb27fe96f3
phuongnamcmu <phuongnamcmg47@gmail.com>
2018-07-26 11:51:02 +0700

    update route

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,5 @@ router.get('/profile', function(req, res) {
     userController.getOneUser(req,res);
   });
 
-  //
-  var bookController = require('../controllers/book');
-  router.get('/user/books', function(req, res) {
-    bookController.getUserBooks(req,res);
-  });
 
-  //
-  router.post('/user/change/info', function(req, res) {
-    userController.updateProfileUser(req,res);
-  });
 module.exports = router;
