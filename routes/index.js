3010474e63bb3dbf79ee7ec70d241bf5aed9db61
phuongnamcmu <phuongnamcmg47@gmail.com>
2018-07-26 11:46:31 +0700

    finish update profile

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,9 @@ router.get('/profile', function(req, res) {
   router.get('/user/books', function(req, res) {
     bookController.getUserBooks(req,res);
   });
+
+  //
+  router.post('/user/change/info', function(req, res) {
+    userController.updateProfileUser(req,res);
+  });
 module.exports = router;
