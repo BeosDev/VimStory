e9258a422e2034f542029575bef2b6c03f4bc80d
beosdev <blhl1210@gmail.com>
2018-07-26 11:34:21 +0700

    update validator login

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
 }));
 app.use(passport.initialize());;
 app.use(passport.session());
-
+app.use(require('connect-flash')());
 
 require('./config/passport')(passport);
 
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,19 @@
 var passport = require('passport');
-    LocalStrategy = require('passport-local').Strategy,
-    UserModel = require('../models/user');
+LocalStrategy = require('passport-local').Strategy,
+    UserModel = require('../models/user'),
+    userSchemaValid = require('./validator').userSchema,
+    Joi = require('joi');
+
+
+function isValided(username, password) {
+    const result = Joi.validate({
+        Username: username,
+        Password: password,
+    }, userSchemaValid);
+    console.log(result);
+    if (result.error) return false;
+    return true;
+}
 
 module.exports = function () {
 
@@ -14,17 +27,21 @@ module.exports = function () {
 
     passport.use('local-login', new LocalStrategy({
         usernameField: 'username',
-        passwordField: 'password'
-    }, function (username, password, done) {
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function (req, username, password, done) {
         var User = new UserModel.getOneUserViaUserName(username);
+        if (!isValided(username, password)) {
+            return done(null, false, req.flash('loginMessage',"Tài khoản hoặc mật khẩu không hợp lệ"));
+        }
         User.once('results', function (results) {
-            if (results.length !== 0 && UserModel.validPassword(password,results[0].Password)){
-                return done(null,results[0]);
+            if (results.length !== 0 && UserModel.validPassword(password, results[0].Password)) {
+                return done(null, results[0]);
             }
-            return done(null, false);
+            return done(null, false, req.flash('loginMessage', 'Tài khoản hoặc mật khẩu không đúng, xin vui lòng kiểm tra lại'));
         })
         User.once('error', function (err) {
-            return done(null, false);
+            return done(null, false, req.flash('loginMessage', 'Tài khoản hoặc mật khẩu không đúng, xin vui lòng kiểm tra lại'));
         })
     }))
 
