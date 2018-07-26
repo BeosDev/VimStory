f726c454b7d133b1b0596e034067f710b3ff6df7
beosdev <blhl1210@gmail.com>
2018-07-26 16:24:18 +0700

    add like

---
+++ models/like.js
@@ -0,0 +1,34 @@
+var executeQuery = require("../config/database");
+
+function countLikes() {
+    var query = 'SELECT count(*) FROM u_like';
+    return new executeQuery(query);
+}
+
+function getLikeByBid(id){
+    var query = 'SELECT * FROM u_like WHERE B_ID = '+id;
+    return new executeQuery(query);
+}
+
+function addLike(paramters) {
+    var query = 'INSERT INTO u_like SET ?;';
+    return new executeQuery(query, paramters);
+}
+
+function removeLike(bid,uid){
+    var query = `DELETE FROM u_like WHERE B_ID = ${bid} AND U_ID = ${uid}`;
+    return new executeQuery(query);
+}
+
+function isLike(bid,uid){
+    var query = `SELECT * FROM u_like WHERE B_ID = ${bid} AND U_ID = ${uid}`;
+    return new executeQuery(query);
+}
+
+module.exports = {
+    countLikes,
+    addLike,
+    getLikeByBid,
+    removeLike,
+    isLike
+}
