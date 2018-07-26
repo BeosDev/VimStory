fd93fdd3242476409a68d1b08d033cf14da6cf2e
phuongnamcmu <phuongnamcmg47@gmail.com>
2018-07-26 10:42:08 +0700

    add show user profile backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var router = require('express').Router();;
+var userController = require('../controllers/user');
 
 router.use('/',require('./home'));
 router.use('/login',require('./login'));
 router.use('/logout',require('./logout'));
 router.use('/book',require('./book'));
 router.use('/admin',require('./admin'));
-
+router.get('/profile', function(req, res) {
+    userController.getOneUser(req,res);
+  });
 module.exports = router;
